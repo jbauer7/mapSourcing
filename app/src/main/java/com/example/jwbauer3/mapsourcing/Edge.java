f707383038ba9682ea88d264cd7487635e4863dd
jbauer7 <jwbauer3@wisc.edu>
2015-10-18 17:51:34 -0500

    update Edge and Node classes

--- app/src/main/java/com/example/jwbauer3/mapsourcing/Edge.java
+++ app/src/main/java/com/example/jwbauer3/mapsourcing/Edge.java
@@ -9,17 +9,21 @@ public class Edge {
 
 
     private Node start, end;
+    private int weight, direction;
 
     public Edge(Node start, Node end){
         this.start = start;
         this.end = end;
+        weight = 0;
+        direction =0;
     }
 
-    public Node getStart() {
-        return start;
-    }
+    public Node getStart() {return start;}
+    public Node getEnd() {return end;}
+    public int getWeight(){return weight;}
+    public int getDirection(){return direction;}
 
-    public Node getEnd() {
-        return end;
-    }
+    public void setWeight(int weight){this.weight=weight;}
+    public void setDirection(int direction){this.direction= direction;}
+
 }
--- app/src/main/java/com/example/jwbauer3/mapsourcing/Node.java
+++ app/src/main/java/com/example/jwbauer3/mapsourcing/Node.java
@@ -1,20 +1,24 @@
 package com.example.jwbauer3.mapsourcing;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nikhil on 10/11/2015.
  */
 public class Node {
 
     private int xPos, yPos;
+    private ArrayList<Edge> edges;
     public Node(int xPos, int yPos){
         this.xPos = xPos;
         this.yPos = yPos;
+        edges = new ArrayList<Edge>();
     }
-    public int getxPos(){
-        return xPos;
-    }
-    public int getyPos(){
-        return  yPos;
-    }
+    public int getxPos(){return xPos;}
+    public int getyPos(){return  yPos;}
+
     //could store radius, color, pixel area coverage.
+    public ArrayList<Edge> getEdges(){return edges;}
+    public void setEdges(Edge e){this.edges.add(e);}
+
 }
