5ee63b294cce2a76bb6b985dc4839460669e9053
Nikhil Jain <blockerdude2@yahoo.com>
2015-12-09 17:23:35 -0600

    Added a new MainMenu with XML to match. Removed MapActivity.

---
+++ app/src/main/java/com/example/jwbauer3/mapsourcing/MainMenu.java
@@ -0,0 +1,30 @@
+package com.example.jwbauer3.mapsourcing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void navigatePressed(View view){
+        //start activity with mapping enabled
+        Intent mapIntent = new Intent(this, MainActivity.class);
+        startActivity(mapIntent);
+    }
+    public void mapPressed(View view){
+        //start activity with mapping disabled.
+        Intent mapIntent = new Intent(this, MainActivity.class);
+        startActivity(mapIntent);
+    }
+    public void addBuilding(View view){
+        Toast.makeText(this, "Not enabled", Toast.LENGTH_SHORT).show();
+    }
+}
