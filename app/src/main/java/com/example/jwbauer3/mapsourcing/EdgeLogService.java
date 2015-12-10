d47d9ec7e0ee38d8c9ac71b563330ebd6cc5b3ea
erickoth <ekoth@wisc.edu>
2015-12-09 21:58:16 -0600

    Resolving a merge issue, accepted theirs in the merge to avoid any conflicts.

--- app/src/main/java/com/example/jwbauer3/mapsourcing/EdgeLogService.java
+++ app/src/main/java/com/example/jwbauer3/mapsourcing/EdgeLogService.java
@@ -439,9 +439,9 @@ public class EdgeLogService extends Service {
     ///////////////////////////////////////// Navigation Methods////////////////////////////////////////
     private void navigationHandler(float newDegree){
         int direction;
-        Edge closestEdge=null;
+        BaseEdge closestEdge=null;
         BaseNode endNode;
-        for(Edge currEdge: currNode.getEdges()){
+        for(BaseEdge currEdge: currNode.getEdges()){
             direction=currEdge.getDirection();
             if(!currEdge.getStart().equals(currNode)) direction=(direction+180)%360;
             if(Math.abs(currEdge.getDirection()-newDegree)<45){
