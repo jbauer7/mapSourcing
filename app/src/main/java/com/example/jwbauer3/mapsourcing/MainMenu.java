7a1ba4f9932268b05079c1340011cb56d719ce4a
Nikhil Jain <blockerdude2@yahoo.com>
2015-12-09 18:05:06 -0600

    Fixed bug not allowing locationnodes to persist after floor switching. Some code refactoring.

--- app/src/main/java/com/example/jwbauer3/mapsourcing/MainMenu.java
+++ app/src/main/java/com/example/jwbauer3/mapsourcing/MainMenu.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainMenu extends Activity {
+
+    private String[] buildingNames = {"Engineering Hall", "Computer Aided Engineering", "Engineering Centers Building"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setUpSpinner();
     }
 
     public void navigatePressed(View view){
@@ -27,4 +33,22 @@ public class MainMenu extends Activity {
     public void addBuilding(View view){
         Toast.makeText(this, "Not enabled", Toast.LENGTH_SHORT).show();
     }
+
+    private void setUpSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.Spinner_MainMenu_BuildingSelect);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, buildingNames);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //does nothing atm
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
+    }
+
 }
